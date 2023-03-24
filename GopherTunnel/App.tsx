0e9a86af74321d23b8ec001d7693a8aa003be0c5
lnelson08 <lukenelson9714@gmail.com>
2023-03-23 19:43:20 -0500

    Added text input, Added buttons for navigation

--- GopherTunnel/App.tsx
+++ GopherTunnel/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createDrawerNavigator, DrawerItem} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {IconButton} from 'react-native-paper';
 import MenuIcon from './components/MenuIcon';
+import DrawerContent from './components/DrawerContent';
 import Settings from './Settings';
 import Home from './Home';
 
@@ -14,8 +16,16 @@ export default function App() {
   return (
     <SafeAreaProvider>
         <NavigationContainer>
-          <Drawer.Navigator screenOptions={{headerShown: true, headerLeft: () => <MenuIcon />}}>
-            <Drawer.Screen name='Home' component={Home} />
+          <Drawer.Navigator 
+            screenOptions={{headerShown: false}}
+            drawerContent={(props) => <DrawerContent {...props} />}
+            >
+            <Drawer.Group
+              screenOptions= {{ headerTitle : "Locations" }}>
+                <Drawer.Screen name='Home' component={Home} />
+
+                
+            </Drawer.Group>
             <Drawer.Screen name='Settings' component={Settings} />
           </Drawer.Navigator>
         </NavigationContainer>
--- GopherTunnel/Home.tsx
+++ GopherTunnel/Home.tsx
@@ -1,23 +1,63 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {Title} from 'react-native-paper';
+import {Title, TextInput, Button} from 'react-native-paper';
+import MenuIcon from './components/MenuIcon';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import base from './styles/base';
 
 interface IHomeProps {}
 
-const Home: React.FunctionComponent<IHomeProps> = (props) => {
+function displayRoute(start : string, end : string) {
+  
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <View>
+      <Text>
+        Start: {start}
+      </Text>
+      <Text>
+        End: {end}
+      </Text>
     </View>
+  )
+}
+
+
+const Home: React.FunctionComponent<IHomeProps> = (props) => {
+  const [start, setStart] = React.useState("");
+  const [end, setEnd] = React.useState("");
+  const [pressed, setPressed] = React.useState(false);
+
+  let route = '';
+  if (pressed) {
+    route = 'Placeholder';
+  }
+  
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <MenuIcon />
+      <View style={base.search}>
+        <TextInput
+          label="Start"
+          value={start}
+          onChangeText={start => setStart(start)}
+        />
+        <TextInput
+          label="End"
+          value={end}
+          onChangeText={end => setEnd(end)}
+        />
+        <View style={base.routeButton}>
+          <Button mode="contained" onPress={() => setPressed(pressed => !pressed)}>
+              Find Route
+          </Button>
+        </View>
+      </View>
+      <View style={base.centered}>
+        <Text>
+          {route}
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 export default Home;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
